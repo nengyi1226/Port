83371d938c7b868e283a410ee5149944764243fa
nengyi1226 <124417045+nengyi1226@users.noreply.github.com>
2023-07-01 09:42:08 -0700

    Update Close the remote port.bat

--- Close the remote port.bat
+++ Close the remote port.bat
@@ -1,9 +1,1 @@
-@echo off
-netsh advfirewall firewall add rule name="Block_RDP" dir=in protocol=TCP localport=3389 action=block
-netsh advfirewall firewall add rule name="Block_SMB" dir=in protocol=TCP localport=445 action=block
-netsh advfirewall firewall add rule name="Block_NetBIOS" dir=in protocol=UDP localport=137-138 action=block
-netsh advfirewall firewall add rule name="Block_NetBIOS" dir=in protocol=TCP localport=139 action=block
-netsh advfirewall firewall add rule name="Block_WinRM" dir=in protocol=TCP localport=5985 action=block
-netsh advfirewall firewall add rule name="Block_WinRM" dir=in protocol=TCP localport=5986 action=block
-echo All specified ports have been blocked.
-pause
+Sorry source code modification, not open program download, please look forward to.
